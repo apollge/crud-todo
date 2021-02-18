123fb291de149a8d3198f39707e68fafb9a86608
Jan G <apollg3@gmail.com>
2021-02-18 13:54:09 +0800

    Fix for regenerationRuntion not defined

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import "antd/dist/antd.css";
-
+import React from "react";
 import { client } from "../services/ApolloClient";
 import "../styles/custom.css";
 import "../styles/global.css";
